3be4f5da6038ee1a5382cac41e5a50c07ce0af19
andreivercosa <andreivercosa@gmail.com>
2018-10-28 00:18:52 -0400

    v1

--- idade/Program.cs
+++ idade/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace idade
 {
@@ -6,7 +7,28 @@ namespace idade
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int idade, maiorIdade = 0, menorIdade = 0;
+            for(int i = 1; i<=3; i++)
+            {
+                Console.WriteLine("Informe a idade: ");
+                idade = Int32.Parse(Console.ReadLine());
+
+                if (idade >= 18)
+                {
+                    maiorIdade++;
+                }
+                else
+                {
+                    menorIdade++;
+                }              
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ha {0} pessoa(s) maior(es) de idade", maiorIdade);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Ha {0} pessoa(s) menor(es) de idade", menorIdade);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
